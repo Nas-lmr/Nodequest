9a24f8e6f5ac8707858df44519507872cbc66a21
Nas-lmr <nasreddine-lamamra_student2023@wilder.school>
2023-10-30 09:18:43 +0100

    add dotenv

---
+++ app.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+require("dotenv").config();
+
+const name = process.env.MY_NAME;
+const city = process.env.MY_CITY;
+const language = process.env.MY_LANGUAGE;
+
+console.log(
+  `I am ${name}, wilder in ${city}, and I love ${language}.`
+);
